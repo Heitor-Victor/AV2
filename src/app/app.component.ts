5e39fae323ddf7b1b0be8fb888988870c2ee1115
AlexandreMPS <alexandre.mps20@outlook.com>
2023-12-03 15:41:08 -0300

    Exercício 04 OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContadorComponentComponent } from './components/exercicio-1/contador-component/contador-component.component';
 import { ListItemsComponent } from './components/exercicio-2-e-3/list-items/list-items.component';
+import { FormCadastroComponent } from './components/exercicio-4/form-cadastro/form-cadastro.component';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContadorComponentComponent, ListItemsComponent],
+  imports: [CommonModule, RouterOutlet, ContadorComponentComponent, ListItemsComponent, FormCadastroComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/components/exercicio-4/form-cadastro/form-cadastro.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-cadastro',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './form-cadastro.component.html',
+  styleUrl: './form-cadastro.component.css',
+})
+export class FormCadastroComponent {
+
+  CadastroForm!: FormGroup;
+  checkCampos: number = 0;
+  userName: string = '';
+  constructor(private formBuilder: FormBuilder) {
+    this.CadastroForm = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      password: [''],
+    });
+  }
+  CadastrarUser(event: Event) {
+    if (this.CadastroForm.value.email !== '' && this.CadastroForm.value.password !== '' && this.CadastroForm.value.name !== '') {
+      const newUser: {
+        name: string;
+        email: string;
+        password: string;
+      } = this.CadastroForm.value;
+      this.CadastroForm.reset();
+      console.log(newUser);
+      this.checkCampos = 1;
+      this.userName = newUser.name;
+      event.preventDefault();
+    } else {
+      this.checkCampos = 2;
+    }
+  }
+}
